8e6fa90f189c5b4c837bedee9efeedd174370cd7
kenny.hao.g@gmail.com <kenny.hao.g@gmail.com>
2019-10-22 14:58:13 +0800

    上传了 pom.xml 如果要写类，请自行建立自己的分支，禁止在 master 中随意上传文件

---
+++ src/test/java/com/iceland/springboot/IceLandMilkTeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iceland.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IceLandMilkTeaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
