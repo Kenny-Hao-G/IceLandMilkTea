6dbfeed747d59f716bfc69256d9c162cc4794078
kenny.hao.g@gmail.com <kenny.hao.g@gmail.com>
2019-10-22 15:09:29 +0800

    上传了 pom.xml 如果要写类，请自行建立自己的分支，禁止在 master 中随意上传文件!!

---
+++ src/main/java/com/iceland/springboot/IceLandMilkTeaApplication.java
@@ -0,0 +1,18 @@
+package com.iceland.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @date 2019-10-22 2:49 下午
+ * @version 1.1
+ * @IceLandMilkTeaApplication SpringBoot启动类
+ */
+@SpringBootApplication
+public class IceLandMilkTeaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IceLandMilkTeaApplication.class, args);
+    }
+
+}
