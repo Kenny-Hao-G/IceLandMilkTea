3b9ae94d2425304d109595fdcfd590c317a8f67e
1073942427@qq.com <1073942427@qq.com>
2019-10-25 17:42:09 +0800

    登录1122

--- src/main/java/com/iceland/springboot/controller/UserController.java
+++ src/main/java/com/iceland/springboot/controller/UserController.java
@@ -10,7 +10,7 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
 /**
