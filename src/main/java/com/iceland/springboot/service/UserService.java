c0cd2c99337594a24bef0287d2c227ee8fc6dabd
kenny.hao.g@gmail.com <kenny.hao.g@gmail.com>
2019-10-29 15:56:39 +0800

    添加了User 中的接口

--- src/main/java/com/iceland/springboot/service/UserService.java
+++ src/main/java/com/iceland/springboot/service/UserService.java
@@ -3,6 +3,7 @@ package com.iceland.springboot.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.iceland.springboot.pojo.User;
 import com.iceland.springboot.vo.ForgetPassword;
+import io.swagger.models.auth.In;
 
 /**
  * @author WWP
@@ -17,20 +18,41 @@ public interface UserService extends IService<User> {
      * 进入修改用户信息页面，先查询用户信息
      * 根据id 查询用户信息
      *
-     * @param id 页面传入的值
+     * @param userId 页面传入的值
      * @return
      */
-     User queryUserInfo(Integer id);
+    User queryUserInfo(Integer userId);
 
-    /**
-     * 点击编辑后更改用户名
-     *
-     * @param username
-     * @return
-     */
-     String modifyUsername(String username);
+
 
     boolean login(User user);
 
     boolean forgetPassword(ForgetPassword forgetPassword);
+
+    /**
+     * 修改用户名
+     *
+     * @param username 传入的用户名
+     * @return
+     */
+    Integer modifyUsername(User username);
+
+    /**
+     * 修改用户邮箱
+     *
+     * @param userEmail
+     * @return
+     */
+    Integer modifyUserEmail(User userEmail);
+
+    /**
+     * 修改用户邮箱
+     *
+     * @param userPassword 电话号码
+     * @param id 用户 id
+     * @return
+     */
+    boolean security(Integer id,String userPassword);
+
+    boolean queryUserPassword(Integer id, String userPassword);
 }
--- src/main/java/com/iceland/springboot/service/impl/UserServiceImpl.java
+++ src/main/java/com/iceland/springboot/service/impl/UserServiceImpl.java
@@ -9,6 +9,7 @@ import com.iceland.springboot.utils.MailUtils;
 import com.iceland.springboot.utils.Md5Utils;
 import com.iceland.springboot.vo.ForgetPassword;
 import com.iceland.springboot.vo.Result;
+import io.swagger.models.auth.In;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
@@ -46,7 +47,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
     @Override
     public boolean login(User user) {
 
-        if (user.getUserEmail() != null || user.getUserName() != null || user.getUserPhoneNumber() != null){
+        if (user.getUserEmail() != null || user.getUserName() != null || user.getUserPhoneNumber() != null) {
             user.setUserPassword(Md5Utils.getMd5Str(user.getUserPassword()));
             if (user.getUserEmail() != null) {
                 User user1 = getBaseMapper().selectOne(new QueryWrapper<User>().eq("user_email", user.getUserEmail()));
@@ -55,35 +56,38 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
 
                     UsernamePasswordToken token = new UsernamePasswordToken(user.getUserEmail(), user.getUserPassword());
                     subject.getSession().setAttribute("USERNAME", user);
+                    subject.login(token);
                     return true;
 
                 }
 
             }
-        if (user.getUserPhoneNumber() != null) {
-            User user1 = getBaseMapper().selectOne(new QueryWrapper<User>().eq("user_phone_number", user.getUserPhoneNumber()));
-            if (Objects.equals(user1.getUserPassword(), user.getUserPassword())) {
-                Subject subject = SecurityUtils.getSubject();
+            if (user.getUserPhoneNumber() != null) {
+                User user1 = getBaseMapper().selectOne(new QueryWrapper<User>().eq("user_phone_number", user.getUserPhoneNumber()));
+                if (Objects.equals(user1.getUserPassword(), user.getUserPassword())) {
+                    Subject subject = SecurityUtils.getSubject();
 
-                UsernamePasswordToken token = new UsernamePasswordToken(user.getUserPhoneNumber(), user.getUserPassword());
-                subject.getSession().setAttribute("USERNAME", user);
-                return true;
+                    UsernamePasswordToken token = new UsernamePasswordToken(user.getUserPhoneNumber(), user.getUserPassword());
+                    subject.getSession().setAttribute("USERNAME", user);
+                    subject.login(token);
+                    return true;
 
-            }
-
-        }
-        if (user.getUserName() != null) {
-            User user1 = getBaseMapper().selectOne(new QueryWrapper<User>().eq("user_name", user.getUserName()));
-            if (Objects.equals(user1.getUserPassword(), user.getUserPassword())) {
-                Subject subject = SecurityUtils.getSubject();
-
-                UsernamePasswordToken token = new UsernamePasswordToken(user.getUserName(), user.getUserPassword());
-                subject.getSession().setAttribute("USERNAME", user);
-                return true;
+                }
 
             }
+            if (user.getUserName() != null) {
+                User user1 = getBaseMapper().selectOne(new QueryWrapper<User>().eq("user_name", user.getUserName()));
+                if (Objects.equals(user1.getUserPassword(), user.getUserPassword())) {
+                    Subject subject = SecurityUtils.getSubject();
 
-        }
+                    UsernamePasswordToken token = new UsernamePasswordToken(user.getUserName(), user.getUserPassword());
+                    subject.getSession().setAttribute("USERNAME", user);
+                    subject.login(token);
+                    return true;
+
+                }
+
+            }
         }
         if (user.getUserPhoneNumber() == null && user.getUserName() == null && user.getUserEmail() == null) {
             return false;
@@ -97,24 +101,14 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
      * 进入修改用户信息页面，先查询用户信息
      * 根据id 查询用户信息
      *
-     * @param id 页面传入的值
+     * @param userId 页面传入的值
      * @return
      */
     @Override
-    public User queryUserInfo(Integer id) {
-        return userMapper.queryUserInfoById(id);
+    public User queryUserInfo(Integer userId) {
+        return userMapper.queryUserInfoById(userId);
     }
 
-    /**
-     *
-     * @param username
-     * @return
-     */
-    @Override
-    public String modifyUsername(String username) {
-        return userMapper.modifyUsername(username);
-
-    }
 
     @Override
     public boolean forgetPassword(ForgetPassword forgetPassword) {
@@ -135,5 +129,52 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
 
         }
         return false;
+    }
+
+    /**
+     * 修改用户名
+     *
+     * @param username 传入的用户名
+     * @return
+     */
+    @Override
+    public Integer modifyUsername(User username) {
+        return userMapper.modifyUsername(username);
+    }
+
+    /**
+     * 修改用户邮箱
+     *
+     * @param userEmail 传入的邮箱名
+     * @return
+     */
+    @Override
+    public Integer modifyUserEmail(User userEmail) {
+        return userMapper.modifyUserEmail(userEmail);
+    }
+
+    /**
+     * 根据用户 id 修改密码
+     *
+     * @param id           用户 id
+     * @param userPassword 电话号码
+     * @return
+     */
+    @Override
+    public boolean security(Integer id, String userPassword) {
+        User user = new User();
+        user.setUserId(id);
+        user.setUserPassword(Md5Utils.getMd5Str(userPassword));
+        int result = userMapper.modifyUserPhoneNum(user);
+        return result > 0 ? true : false;
+    }
+
+    @Override
+    public boolean queryUserPassword(Integer id, String userPassword) {
+        User user = new User();
+        user.setUserId(id);
+        user.setUserPassword(Md5Utils.getMd5Str(userPassword));
+        int result = userMapper.queryUserPassword(user);
+        return result > 0 ? true : false;
     }
 }
