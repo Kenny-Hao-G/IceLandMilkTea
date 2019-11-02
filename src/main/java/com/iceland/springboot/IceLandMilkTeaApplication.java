96d53beb8ed84211ee833d728f55da465a64fcf1
xiwei <2869550771@qq.com>
2019-11-02 19:01:29 +0800

    席的功能模块11

--- src/main/java/com/iceland/springboot/IceLandMilkTeaApplication.java
+++ src/main/java/com/iceland/springboot/IceLandMilkTeaApplication.java
@@ -18,6 +18,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class IceLandMilkTeaApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(IceLandMilkTeaApplication.class, args);
     }
 
